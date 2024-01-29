ba5598900431d178aa23da4ccf45d7fb36f7b18a
ov3rthink <overth1560@gmail.com>
2024-01-29 13:11:25 -0300

    rebuilding the code

--- hashanalyzer.hpp
+++ hashanalyzer.hpp
@@ -1,42 +1,138 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <unordered_set>
 #include <termcolor.hpp>
 
+void HashWarning(void){
+	std::cout << termcolor::on_red <<  "[!] Unidentified hash " << termcolor::reset << std::endl;
+}
+
+void OutputProgram(std::string PossibleHashs){
+	std::cout << termcolor::on_red << "[+] " << PossibleHashs << termcolor::reset << std::endl;
+}
+
+void Hash(const std::string& str) {
+
+	bool UseSpecialChar = false;
+	bool UseLetter = false;
+	bool UseNumbers = false;
+
+	for (char character : str ) {
+		if (std::isalpha(character)) {
+            		UseLetter = true;
+		} else if (std::isdigit(character)) {
+			UseNumbers = true;
+		} else if (!std::isalnum(character)) {
+			UseSpecialChar = true;
+        	} else {
+			HashWarning();
+		}
+	}
 
 
-class Hash {
-public:
-    void SetHashProperties(const std::string& str);
+	std::unordered_set<int> allowedSizes = {4, 8, 13, 16, 32, 34, 37, 48, 49, 50, 52, 56, 57, 58, 68, 118, 119};
 
-    std::string hash;
-    bool numeric;
-    bool useLetter;
-    bool letterLowercase;
-    bool letterUppercase;
-    bool specialChar;
-};
+	if ( allowedSizes.find(str.size()) != allowedSizes.end() and (UseLetter == false and UseNumbers == false)){
+		HashWarning();
+	}
 
-void Hash::SetHashProperties(const std::string& str) {
-    hash = str;
-    numeric = false;
-    useLetter = false;
-    letterLowercase = false;
-    letterUppercase = false;
-    specialChar = false;
+	else { 
+			if (str.size() == 4 ){
+				OutputProgram("[+] LoseLose | CRC-8");
+			}
 
-    for (char character : hash) {
-        if (std::isalpha(character)) {
-            useLetter = true;
-            if (std::isupper(character)) {
-                letterUppercase = true;
-            } else {
-                letterLowercase = true;
-            }
-        } else if (std::isdigit(character)) {
-            numeric = true;
-        } else if (!std::isalnum(character)) {
-            specialChar = true;
-        }
-    }
+			else if(str.size() == 8 ){
+				if ( UseSpecialChar ){ 
+					OutputProgram ("[+] CRC-32 | ADLER-32, CRC-32B | XOR-32 | GHas-32-3 | GHash-32-5" );
+				} else {
+					HashWarning();
+				}
+			}
+
+			else if(str.size() == 13) {
+				if ( UseSpecialChar ){
+					OutputProgram("DES(unix)");
+				} else {
+					HashWarning();
+                                }
+
+			}
+
+			else if (str.size() == 16 ){
+				OutputProgram("[+] MD5(Half) | MD5(Middle) | MySQL");
+			}
+
+			else if (str.size() == 32){
+				OutputProgram("[+] Haval-128 | MD2 |  MD4 |  MD5 | NTLM |  RAdmin v2.x |  RipeMD-128 |  SNEFRU-128 |  Tiger-128 ");
+			}
+
+			else if (str.size() == 34 ){
+				if ( UseSpecialChar ){
+					OutputProgram("[+] MD5 |  Lineage II C4");
+				}
+			}
+
+			else if (str.size() == 37){
+				if ( UseSpecialChar ){
+					OutputProgram("[+] MD5 ");
+				}
+			}
+
+			else if (str.size() == 40){
+				OutputProgram("[+] Haval-160, MySQL5 | MySQL5 - SHA-1 | MySQL 160bit | RipeMD-160 | Tiger-160");			
+			}
+
+			else if (str.size() == 48) {
+				OutputProgram("[+] Haval-160 | Tiger-192 ");
+				
+			}
+
+			else if (str.size() == 49){
+				if ( UseSpecialChar ){
+					OutputProgram("[+] MD5 | Joomla ");
+				}else {
+					HashWarning();
+				}
+			}
+
+			else if (str.size() == 52){
+				OutputProgram("[+] SHA-1 ");
+			}
+
+			else if (str.size() == 56){
+				if ( UseSpecialChar ){
+					OutputProgram("[+]  Haval-224 | SHA-224 | Haval-256 | GOST R 34.11-94 | RipeMD-256 | SNEFRU-256 | SHA-256");
+				} 
+			}
+
+			else if (str.size() == 57){
+				if ( UseSpecialChar ) {
+					OutputProgram("[+]  MD5 | Joomla ");
+				} else {
+					HashWarning();
+				}
+			}
+
+			else if (str.size() == 58){
+				OutputProgram("[+] SHA-256 ");
+			}
+
+			else if (str.size() == 65){
+				OutputProgram("[+] SAM ");
+			}
+
+			else if(str.size() == 118){
+				OutputProgram("[+] RideMD-320 ");
+			}
+
+			else if(str.size() == 119){
+				if (UseSpecialChar ) {
+					OutputProgram("[+] Sha-384 ");
+				} else {
+					HashWarning();
+				}
+			}
+		}
+
 }
--- hashover.cpp
+++ hashover.cpp
@@ -3,32 +3,33 @@
 #include <termcolor.hpp>
 #include "hashanalyzer.hpp"
 
+using namespace termcolor;
+
 int main(int argc, char** argv) {
     // args extras
-    if (std::strcmp(argv[1], "-v") == 0 || std::strcmp(argv[1], "-version") == 0 || std::strcmp(argv[1], "--version") == 0) {
-        std::cout << termcolor::blue << "¬HASH OVER" << termcolor::reset   << std::endl;
+    if (std::strcmp(argv[1], "-v") == 0 or std::strcmp(argv[1], "-version") == 0 or std::strcmp(argv[1], "--version") == 0) {
+        std::cout << termcolor::blue << "¬HASH lyz" << termcolor::reset   << std::endl;
         std::cout << termcolor::blue << "version : 1.0" << termcolor::reset << std::endl;
     }
-    else if (std::strcmp(argv[1], "--help") == 0 || std::strcmp(argv[1], "-h") == 0 || std::strcmp(argv[1], "-help") == 0) {
-        std::cout << termcolor::on_blue << "example : hashover 23jk4f0x9if902cf f89fa232" << termcolor::reset << std::endl;
-        std::cout << termcolor::blue << "¬ dev : overthink\n¬ repository : https://github.com/ov3rthink/hash-ov3r\nfirst version : 24 jan 2024 " << termcolor::reset << std::endl;
+    else if (std::strcmp(argv[1], "--help") == 0 or std::strcmp(argv[1], "-h") == 0 or std::strcmp(argv[1], "-help") == 0) {
+        std::cout << termcolor::on_blue << "¬ ! hash lyz is a tool for analyzing hash algorithm outputs and made in open source C++." << termcolor::reset << std::endl;
+	std::cout << termcolor::blue << "-version -v --version ¬ display the current version of your hash analyzer." << termcolor::reset << std::endl;
+        std::cout << termcolor::blue << "¬ dev : ov3rthink\n¬ repository : https://github.com/ov3rthink/hash-ov3r\nfirst version : 24 jan 2024 " << termcolor::reset << std::endl;
     }
 
     // main
     else {
         for (int i = 1; i < argc; i++) {
-            if (std::strlen(argv[i]) < 8 || std::strlen(argv[i]) > 512) {
+            if (std::strlen(argv[i]) < 4 or std::strlen(argv[i]) > 512) {
                 std::cout << termcolor::on_red << "[!] - argument[" << i << "] : " << argv[i] << " size invalid!!" << termcolor::reset << std::endl;
                 return 1;
             }
         }
 
-        Hash* HashArg = new Hash[argc-1];
-        for (int i = 1; i < argc; ++i) {
-            HashArg[i-1].SetHashProperties(argv[i]);
-        }
-
-        delete[] HashArg;
+        for (int i = 0; i < argc-1; i++ ) {
+		std::cout << termcolor::on_red << "[+]-" << i << " " << argv[i] << termcolor::reset << std::endl;
+		Hash(argv[i]);
+	}
     }
 
     return 0;
